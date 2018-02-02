c41c9399f10cbef51ff2c4e55a43d26b80c70947
bandris342 <andras.bartok@gmail.com>
2018-02-02 18:11:25 +0100

    Add login method

--- webapp/urls.py
+++ webapp/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from articles import views
 
 urlpatterns = [
     url(r'^$', views.articles_list, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^articles/', include('articles.urls', namespace="articles"), name="articles"),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ]
